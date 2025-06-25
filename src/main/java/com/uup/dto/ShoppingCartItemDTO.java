33cd320265a608ed373e057c0590616a743655f8
Mahmoud <maboukhelkhal@gmail.com>
2025-06-24 17:36:32 -0700

    finished Phase 3

--- src/main/java/com/uup/dto/ShoppingCartItemDTO.java
+++ src/main/java/com/uup/dto/ShoppingCartItemDTO.java
@@ -6,13 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.util.Map;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ShoppingCartDTO {
-    private Map<Integer, ShoppingCartItemDTO> items;
-    private BigDecimal total;
+public class ShoppingCartItemDTO {
+    private ProductDTO product;
+    private int quantity;
+    private int discountPercent;
+    private BigDecimal lineTotal;
 }
